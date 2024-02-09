99e2b95acfcdc52730e22856938238874349ee3f
Hamza-Ziani-Dev <hamzaziani.dev@gmail.com>
2024-02-09 16:27:06 +0100

    'done'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
+import { ChakraProvider } from '@chakra-ui/react';
 import './App.css';
 import Posts from './components/Posts';
 
 function App() {
   return (
     <div className="App">
+      <ChakraProvider>
       <h1>Crud Redux</h1>
-        <Posts/>
+      <Posts/>
+      </ChakraProvider>
+        
     </div>
   );
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,7 +1,8 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import { addPost, deletePost, updatePost } from "../features/posts/postSlice";
 import { useDispatch, useSelector } from "react-redux";
 import "./Posts.css";
+import { Button, Center, Input, Stack } from "@chakra-ui/react";
 function Posts() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -12,99 +13,99 @@ function Posts() {
   const [edit, setEdit] = useState(false);
   const [id, setId] = useState(null);
   return (
-    <Fragment>
+    <Center>
       <div className="form">
-        <input
-          type="text"
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-          placeholder="Enter Title"
-          className="inputField"
-        />
-        <input
-          type="text"
-          onChange={(e) => {
-            setDescription(e.target.value);
-          }}
-          placeholder="Enter Description"
-          className="inputField"
-        />
-        <button
+        <Stack spacing={3}>
+          <Input
+            focusBorderColor="pink.400"
+            placeholder="Enter Title ..."
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <Input
+            focusBorderColor="pink.400"
+            placeholder="Enter Description ...."
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+        </Stack>
+        <Button
+          colorScheme="green"
           onClick={() => {
             dispatch(addPost({ id: posts.length + 1, title, description }));
           }}
-          className="SaveButton"
         >
-          Save
-        </button>
-      </div>
+          Add Task
+        </Button>
+        <div className="posts">
+          {posts.length > 0 ? (
+            posts.map((post) => (
+              <div className="post" key={post.id}>
+                <h2>{post.title}</h2>
+                <p>{post.description}</p>
+                <Button
+                  colorScheme="linkedin"
+                  onClick={() => {
+                    setEdit(true);
+                    setId(post.id);
+                  }}
+                >
+                  Update Task
+                </Button>
+                <Button
+                  colorScheme="orange"
+                  onClick={() => {
+                    dispatch(deletePost({ id: post.id }));
+                  }}
+                >
+                  Delete Task
+                </Button>
+                <br />
+                {edit && id === post.id && (
+                  <>
+                    <Stack spacing={3}>
+                      <Input
+                        focusBorderColor="pink.400"
+                        placeholder="Enter Title ..."
+                        onChange={(e) => {
+                          setUpdateTitle(e.target.value);
+                        }}
+                      />
+                      <Input
+                        focusBorderColor="pink.400"
+                        placeholder="Enter Description ...."
+                        onChange={(e) => {
+                          setUpdateDescription(e.target.value);
+                        }}
+                      />
+                    </Stack>
 
-      <div className="posts">
-        {posts.length > 0 ? (
-          posts.map((post) => (
-            <div className="post" key={post.id}>
-              <h2>{post.title}</h2>
-              <p>{post.description}</p>
-              <button
-                onClick={() => {
-                  setEdit(true);
-                  setId(post.id);
-                }}
-                className="updateButton"
-              >
-                Update
-              </button>
-              <button
-                onClick={() => {
-                  dispatch(deletePost({ id: post.id }));
-                }}
-                className="deleteButton"
-              >
-                Delete
-              </button>
-              <br />
-              {edit && id === post.id && (
-                <>
-                  <input
-                    type="text"
-                    onChange={(e) => {
-                      setUpdateTitle(e.target.value);
-                    }}
-                    placeholder="Enter Title"
-                    className="inputField"
-                  />
-                  <input
-                    type="text"
-                    onChange={(e) => {
-                      setUpdateDescription(e.target.value);
-                    }}
-                    placeholder="Enter Description"
-                    className="inputField"
-                  />
-                  <button
-                    onClick={() => {
-                      dispatch(
-                        updatePost({
-                          id: post.id,
-                          title: updateTitle,
-                          description: updateDescription,
-                        })
-                      );
-                    }}
-                    className="updateButton"
-                  >
-                    Update
-                  </button>
-                </>
-              )}
-            </div>
-          ))
-        ) : (
-          <p>There are no posts</p>
-        )}
+                    <Button
+                      colorScheme="messenger"
+                      onClick={() => {
+                        dispatch(
+                          updatePost({
+                            id: post.id,
+                            title: updateTitle,
+                            description: updateDescription,
+                          })
+                        );
+                      }}
+                    >
+                      Update Task
+                    </Button>
+                  </>
+                )}
+              </div>
+            ))
+          ) : (
+            <p>There are no posts</p>
+          )}
+        </div>
       </div>
-    </Fragment>
+    </Center>
   );
 }
 
